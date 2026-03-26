65f2cc24fecfe8b1a6cdecdee3c72df3828d9c84
Pradeep Jangid <37829699+prdpjngd@users.noreply.github.com>
2026-03-27 00:53:08 +0530

    feat: add 480p and 360p encoding profiles

--- config/encoding_profiles.sh
+++ config/encoding_profiles.sh
@@ -1,4 +1,18 @@
+# YouTube-style H.264 (AVC) Bitrates
+# Optimized for high visual quality with small file size
+
+# 1080p (Full HD)
 BITRATE_1080="1350k"
 SCALE_1080="1920:1080"
+
+# 720p (HD)
 BITRATE_720="600k"
 SCALE_720="1280:720"
+
+# 480p (SD)
+BITRATE_480="400k"
+SCALE_480="854:480"
+
+# 360p (Low Quality / Mobile)
+BITRATE_360="250k"
+SCALE_360="640:360"
--- libexec/video/select_resolution.sh
+++ libexec/video/select_resolution.sh
@@ -2,3 +2,5 @@
 HEIGHT="$1"
 [ "$HEIGHT" -ge 1080 ] && echo 1080
 [ "$HEIGHT" -ge 720 ] && echo 720
+[ "$HEIGHT" -ge 480 ] && echo 480
+echo 360 # Always provide 360p as the baseline quality
